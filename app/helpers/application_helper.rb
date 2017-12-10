690dded8d696c34dbfcd01ba3482130fd4f048a3
Fabio Albini <fabio.albini.b@gmail.com>
2017-12-10 15:58:53 -0200

    show flash messages as bootstrap alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def bootstrap_alert_class(type)
+    case type.to_sym
+    when :success then "alert-success"
+    when :error then "alert-danger"
+    when :alert then "alert-warning"
+    when :notice then "alert-info"
+    else
+      type
+    end
+  end
 end
