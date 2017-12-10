735558e52ff49fa84c35159b9d5b68e1b46609b7
Fabio Albini <fabio.albini.b@gmail.com>
2017-12-10 20:53:10 -0200

    removing bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,4 @@
 //
 //= require rails-ujs
 //= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-  def bootstrap_alert_class(type)
-    case type.to_sym
-    when :success then "alert-success"
-    when :error then "alert-danger"
-    when :alert then "alert-warning"
-    when :notice then "alert-info"
-    else
-      type
-    end
-  end
 end
