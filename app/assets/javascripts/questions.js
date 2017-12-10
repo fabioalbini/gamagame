8662f80669acb98a104518ca3252a7c494af1505
Fabio Albini <fabio.albini.b@gmail.com>
2017-12-10 20:55:04 -0200

    avoid multiple submisions for a question response

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,7 +1,15 @@
 var ready = function() {
+
   $("input[type=radio]").click(function() {
+    $("#btn-next-question").removeAttr("disabled");
     $("#btn-next-question").removeClass("disabled");
-  })
+  });
+
+  $("#btn-next-question").click(function() {
+    $(this).addClass("disabled");
+    $(this).attr("disabled", "disabled");
+    $(this).parents("form").submit();
+  });
 };
 
 $(document).ready(ready);
